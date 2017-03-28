6b73fb6ed2d8cb44bda02807e013e6a8aad70dca
Brooks Patton <brooks.patton@galvanize.com>
2017-03-28 15:48:46 -0600

    searching omdb

--- public/main.js
+++ public/main.js
@@ -1,7 +1,21 @@
+var apiUrl = 'http://www.omdbapi.com/?'
+
 $(document).ready(function() {
 	$('form').submit(function(event) {
 		event.preventDefault()
 
-		console.log('form submitted')
+		var searchTerm = $('input').val()
+
+		searchOMDB(searchTerm)
 	})
 })
+
+function searchOMDB(term) {
+	$.get(apiUrl + 's=' + term)
+	.then(function(data) {
+		console.log(data)
+	})
+	.catch(function(err) {
+		console.error(err)
+	})
+}
